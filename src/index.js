8afe25a59f863922f9baf171358950bb64e7d699
Masroor Hamdani <masroorhamdani@Masroors-MacBook-Air.local>
2018-11-22 14:56:27 +0800

    Added support for multiple languages, en and singapore chinese

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './sass/index.css';
 import App from './App';
 import { I18nextProvider, withNamespaces } from "react-i18next";
-
 import i18n from "./i18n.js";
+
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
@@ -12,22 +12,9 @@ import i18n from "./i18n.js";
 // Learn more about service workers: http://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 
-// ReactDOM.render(
-//     <I18nextProvider i18n={i18next}>
-//         <App/>
-//     </I18nextProvider>,
-//     document.getElementById('root')
-// );
-
-// const AppWithI18n = withI18n()(App); // pass `t` function to App
-// // append app to dom
-// ReactDOM.render(
-//   <AppWithI18n />,
-//   document.getElementById("root")
-// );
 ReactDOM.render(
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>,
-    document.getElementById("root")
-  );
+  <I18nextProvider i18n={i18n}>
+    <App />
+  </I18nextProvider>,
+  document.getElementById("root")
+);
