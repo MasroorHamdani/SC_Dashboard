138a56405d08bd3d6c2504c8430c153b3df0335e
Masroor Hamdani <masroorhamdani@Masroors-MacBook-Air.local>
2018-11-27 18:57:26 +0800

    Login page Design

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,53 +1,62 @@
 import React from 'react';
 import { NamespacesConsumer } from 'react-i18next';
 import "./../sass/App.css";
+import "./../sass/Login.css";
+
 class LoginComponent extends React.Component {
+  
+  state = {
+    usernameCss: '',
+    passwordCss: ''
+  }
     render() {
       const {username, password} = "this.props;"
-        return (
-          <div className="auth-page">
-          
-            <div className="container page">
-              <div className="row">
-                <NamespacesConsumer>{
-                  t => <div className="col-md-6 offset-md-3 col-xs-12">
-                    <h1 className="text-xs-center">{t('signin')}</h1>
-                    <form>
-                      <fieldset>
-                        <fieldset className="form-group">
-                          <input
-                            className="form-control form-control-lg"
-                            type="text"
-                            placeholder={t('username')}
-                            name="username"
-                            value={username}
-                            onChange={this.props.onChange}
-                            />
-                        </fieldset>
-                        <fieldset className="form-group">
-                          <input
-                            className="form-control form-control-lg"
-                            type="password"
-                            placeholder={t('password')}
-                            name="password"
-                            value={password}
-                            onChange={this.props.onChange}
-                            />
-                        </fieldset>
-                        <input
-                          className="btn btn-lg btn-primary pull-xs-right"
-                          type="button"
-                          value={t('signin')}
-                          onClick={this.props.onClick}
-                        />
-                      </fieldset>
-                    </form>
+      return(
+        <NamespacesConsumer>{
+        t=><div className="container-box-login">
+            <div className="box-border">
+                <div>
+                  <div className="row">
+                    <div className="login-header">
+                    
+                      <form className="login-form-resp dosis-semibold login-form-boundary login-form-resp-login">
+                        <div className="login-div">
+                          <div className="group group-login">
+                            <div className="email-field">
+                              <input type="text" name="username"
+                              className={this.state.usernameCss}
+                              onClick={this.setClass.bind(this, 'usernameCss')} onChange={this.props.onChange}/><span className="highlight"></span><span className="bar"></span>
+                              <label>{t('username')}</label>
+                            </div>
+                          </div>
+                          <div className="group group-login">
+                            <div className="password-field">
+                              <input type="password" name="password"
+                              className={this.state.passwordCss}
+                              onChange={this.props.onChange}
+                              onClick={this.setClass.bind(this, 'passwordCss')}/><span className="highlight"></span><span className="bar"></span>
+                              <label>{t('password')}</label>
+                            </div>
+                          </div>
+                          <div className="get-full-width-login">
+                          <div>
+                            <button type="button" className="btn btn-success login-bttn-login button buttonBlue" onClick={this.props.onClick}>{t('signin')}
+                              <div className="ripples buttonRipples"><span className="ripplesCircle"></span></div>
+                            </button>
+                            </div>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
                   </div>
-                }</NamespacesConsumer>
               </div>
             </div>
-          </div>
+        </div>
+        }</NamespacesConsumer>
         );
       }
+      setClass = (value) => {
+        this.setState({[value]: "used" });
+      };
 }
 export default LoginComponent;
