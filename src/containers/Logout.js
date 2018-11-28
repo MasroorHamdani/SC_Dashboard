413ca6cdd2a411cd54230228ba0955d80dbf2356
Masroor Hamdani <masroorhamdani@Masroors-MacBook-Air.local>
2018-11-28 14:34:25 +0800

    Changes in logout file

--- src/containers/Logout.js
+++ src/containers/Logout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { REACT_URLS } from "../constants/Constant";
 
 export default class Logout extends React.Component {
-    render() {
+    componentWillMount() {
         localStorage.clear();
         window.location = REACT_URLS['LOGIN'];
         // this.props.history.push(REACT_URLS['LOGIN']);
+      }
+    render() {
         return null
     }
 }
