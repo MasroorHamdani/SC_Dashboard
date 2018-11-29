bdb5d435283e1ed4316b98e41a02095b857b9d33
Masroor Hamdani <masroorhamdani@Masroors-MacBook-Air.local>
2018-11-29 18:50:25 +0800

    Removed unused CSS code and files

--- src/components/commonHeader/CommonHeader.js
+++ src/components/commonHeader/CommonHeader.js
@@ -8,7 +8,6 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import styles from "./CommonHeaderStyle";
-import '../../sass/CommonHeader.scss';
 import { Link} from "react-router-dom";
 import {REACT_URLS} from '../../constants/Constant';
 
