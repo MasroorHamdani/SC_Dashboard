f8312b57e9a6aa263c06af0885270d43b7f4e7b3
Masroor Hamdani <masroorhamdani@Masroors-MacBook-Air.local>
2018-11-30 12:48:15 +0800

    Routing condition change, removed unsed css from project details, accepted param in project details page, design changes for no match page with return statement

---
+++ src/containers/nomatch/NoMatch.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+import {withStyles, Typography} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { REACT_URLS } from "../../constants/Constant";
+import styles from './NoMatchStyle';
+
+class NoMatch extends Component {
+    render() {
+        const { classes } = this.props;
+        if (!localStorage.getItem('idToken')) {
+            this.props.history.push(REACT_URLS['LOGIN']);
+        } else {
+            return (
+                <div className={classes.root}>
+                <main className={classes.content}>
+                  <Typography component="div">
+                  <div>404 page not found</div>
+                  </Typography>
+                </main>
+              </div>
+            )
+        }
+    }
+}
+
+NoMatch.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(NoMatch);
---
+++ src/containers/nomatch/NoMatchStyle.js
@@ -0,0 +1,28 @@
+import { FormHelperText } from "@material-ui/core";
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+    main: {
+        width: 'auto',
+        display: 'block', // Fix IE 11 issue.
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+          width: 400,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        },
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+        marginTop: theme.spacing.unit * 8,
+        height: '100vh',
+        overflow: 'auto',
+    },
+});
+
+export default styles;
