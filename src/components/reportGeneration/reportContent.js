da91b50a82ec5114bd19855d89cc6b2ea402794b
Masroor Hamdani <masroorhamdani@Masroors-MacBook-Air.local>
2018-12-27 20:08:04 +0800

    Reports page implementation for location

--- src/components/reportGeneration/reportContent.js
+++ src/components/reportGeneration/reportContent.js
@@ -3,7 +3,10 @@ import {withStyles, Card, CardHeader, Avatar,
     IconButton, GridList, Checkbox, Button,
     FormControl, InputLabel, NativeSelect,
     FormHelperText, Input, Grid, CardActions,
-    Collapse, CardContent, Typography} from '@material-ui/core';
+    Collapse, CardContent, ListItem, ListItemText,
+    ListItemSecondaryAction, List, GridListTile, GridListTileBar,
+    ExpansionPanel, ExpansionPanelSummary, Typography, ExpansionPanelDetails} from '@material-ui/core';
+// import {GridTile} from 'material-ui/GridList';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import classnames from 'classnames';
 import {REPORT_TABS, SERVICES} from '../../constants/Constant';
@@ -11,15 +14,130 @@ import styles from './ReportGenerationStyle';
 
 class ReportContent extends Component {
     state = { };
-    handleExpandClick = (index, value) => {
-        this.setState({ [index]: !value });
-        // this.setState(state => ({ [index]: !state[index] }));
-    };
+    
     render() {
         const {type, classes, data, stateData,
-            handleProjectSelectionChange} = this.props;
+            handleProjectSelectionChange, handleExpandClick,
+            handleDeviceToggle, onNextClick, onPreviousClick,
+            handleServiceToggle} = this.props;
         return(
             <div>
+                {type === REPORT_TABS['LOCATION'] &&
+                    <div>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel shrink htmlFor="project-native-label-placeholder">
+                                Project List
+                            </InputLabel>
+                            <NativeSelect
+                                value={stateData.project}
+                                onChange={handleProjectSelectionChange}
+                                input={<Input name="project" id="project-native-label-placeholder" />}>
+                                <option value='' key='project' id='project'>Choose Project</option>
+                                { (data.DashboardReducer.data && data.DashboardReducer.data.Projects) &&
+                                    data.DashboardReducer.data.Projects.map(function(dt) {
+                                        return <option value={dt.pid} key={dt.pid}>{dt.name}</option>
+                                    })
+                                }
+                            </NativeSelect>
+                            <FormHelperText>Please Select a Project for Report generationr</FormHelperText>
+                        </FormControl>
+                        {data.ProjectDetailsReducer.data &&
+                            // <div className={classnames(classes.gridRoot, classes.gridList)}>
+                            <div>
+                                {data.ProjectDetailsReducer.data.map((row, index) => {
+                                    return <ExpansionPanel key={index}>
+                                    <ExpansionPanelSummary
+                                        onClick={event => handleExpandClick(index, row.insid, row.PID)}
+                                        expandIcon={<ExpandMoreIcon/>}>
+                                        <div className={classes.column}>
+                                            <Typography className={classes.heading}>{row.name}</Typography>
+                                        </div>
+                                        <div className={classes.column}>
+                                            <Typography className={classes.secondaryHeading}>Select Sensor Location</Typography>
+                                        </div>
+                                    </ExpansionPanelSummary>
+                                    <ExpansionPanelDetails>
+                                    <div className={classes.column} />
+                                    <div>
+                                        {/* <List dense> */}
+                                        <GridList>
+                                        {stateData[row.insid] &&
+                                            stateData[row.insid].details.map((dt, index) => {
+                                                return <GridListTile key={index}>
+                                                    <div>{dt.id}</div>
+                                                    <Checkbox
+                                                        checked={stateData.deviceChecked.indexOf(dt.id) !== -1}/>
+                                                    </GridListTile>
+
+                                                
+                                            // return<ListItem button key={index} button onClick={handleDeviceToggle(dt.id)}>
+                                            //         <ListItemText primary={dt.id} />
+                                            //         <Checkbox
+                                            //             checked={stateData.deviceChecked.indexOf(dt.id) !== -1}
+                                            //         />
+                                            //     </ListItem>
+                                            })
+                                        }
+                                        {/* </List> */}
+                                        </GridList>
+                                    </div>
+                                    </ExpansionPanelDetails>
+                                  </ExpansionPanel>
+                                    // return <Card className={classes.card} key={index}>
+                                    //     <CardHeader avatar={
+                                    //         <Avatar aria-label="Recipe" className={classes.avatar}>
+                                    //             {row.name.split(' ').map((val) => {
+                                    //                 return (val.substr(0,1))
+                                    //         })}</Avatar>}
+                                    //         title={row.name}
+                                    //     subheader={row.locn}/>
+                                    //     <CardActions className={classes.actions} disableActionSpacing>
+                                    //         <IconButton
+                                    //             className={classnames(classes.expand, {
+                                    //             [classes.expandOpen]: stateData[index],
+                                    //             })}
+                                    //             onClick={event => handleExpandClick(index, row.insid, row.PID)}>
+                                    //             <ExpandMoreIcon />
+                                    //         </IconButton>
+                                    //     </CardActions>
+                                    //     <Collapse in={stateData[index]}
+                                    //         timeout="auto" unmountOnExit
+                                    //         className={classes.collapse}>
+                                    //         {stateData[row.insid] &&
+                                    //         <CardContent>
+                                    //             <List dense>
+                                    //             {stateData[row.insid].details.map((dt, index) => {
+                                    //                  return<ListItem button key={index} button onClick={handleDeviceToggle(dt.id)}>
+                                    //                     <ListItemText primary={dt.id} />
+                                    //                     <Checkbox
+                                    //                         checked={stateData.deviceChecked.indexOf(dt.id) !== -1}
+                                    //                     />
+                                    //                 </ListItem>
+                                    //             })}
+                                    //             </List>
+                                    //         </CardContent>
+                                    //         }
+                                    //     </Collapse>
+                                    // </Card>
+                                })}
+                            </div>
+                        }
+                        <Grid item
+                            container
+                            alignItems='center'
+                            direction='row'
+                            justify='flex-end'
+                            >
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={onNextClick}
+                                >
+                                Next
+                            </Button>
+                        </Grid>
+                    </div>
+                }
                 {type === REPORT_TABS['SERVICE'] &&
                     <div className={classes.gridRoot}>
                         <GridList cellHeight={130} className={classes.gridList}>
@@ -33,7 +151,9 @@ class ReportContent extends Component {
                                     }
                                     action={
                                         <IconButton>
-                                            <Checkbox/>
+                                            <Checkbox
+                                            checked={stateData.serviceChecked.indexOf(SERVICES[key]['id']) !== -1}
+                                            onChange={handleServiceToggle(SERVICES[key]['id'])}/>
                                         </IconButton>
                                     }
                                     title={SERVICES[key]['display']}
@@ -47,73 +167,47 @@ class ReportContent extends Component {
                             justify="space-between"
                             alignItems='center'>
                             <Grid item>
-                                <Button variant="contained" color="primary" className={classes.button}>
+                                <Button
+                                    onClick={onPreviousClick}
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.button}>
                                     Previous
                                 </Button>
                             </Grid>
                             <Grid item>
-                                <Button variant="contained" color="primary" className={classes.button}>
+                                <Button
+                                    onClick={onNextClick}
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.button}>
                                     Next
                                 </Button>
                             </Grid>
                         </Grid>
                     </div>
                 }
-                {type === REPORT_TABS['LOCATION'] &&
-                    <div>
-                        <FormControl className={classes.formControl}>
-                            <InputLabel shrink htmlFor="project-native-label-placeholder">
-                                Project List
-                            </InputLabel>
-                            <NativeSelect
-                                value={stateData.project}
-                                onChange={handleProjectSelectionChange}
-                                input={<Input name="project" id="project-native-label-placeholder" />}>
-                                <option value='test' key='test' id='test'>test</option>
-                                { (data.DashboardReducer.data && data.DashboardReducer.data.Projects) &&
-                                    data.DashboardReducer.data.Projects.map(function(dt) {
-                                        return <option value={dt.pid} key={dt.pid}>{dt.name}</option>
-                                    })
-                                }
-                            </NativeSelect>
-                            <FormHelperText>Please Select the Project to generate Report for</FormHelperText>
-                        </FormControl>
-                        {data.ProjectDetailsReducer.data &&
-                            <div className={classnames(classes.gridRoot, classes.gridList)}>
-                                {data.ProjectDetailsReducer.data.map((row, index) => {
-                                    return <Card className={classes.card} key={index}>
-                                        <CardHeader avatar={
-                                            <Avatar aria-label="Recipe" className={classes.avatar}>
-                                                {row.name.split(' ').map((val) => {
-                                                    return (val.substr(0,1))
-                                                    })}
-                                            </Avatar>}
-                                            title={row.name}
-                                            subheader={row.locn}/>
-                                        <CardActions className={classes.actions} disableActionSpacing>
-                                            <IconButton
-                                                className={classnames(classes.expand, {
-                                                [classes.expandOpen]: this.state[index],
-                                                })}
-                                                onClick={event => this.handleExpandClick(index, this.state[index])}
-                                            >
-                                                <ExpandMoreIcon />
-                                            </IconButton>
-                                            </CardActions>
-                                            <Collapse in={this.state[index]} timeout="auto" unmountOnExit>
-                                            <CardContent>
-                                            <Typography paragraph>Method:</Typography>
-                                            </CardContent>
-                                            </Collapse>
-                                    </Card>
-                                })}
-                            </div>
-                        }
-                        
-                    </div>
+                {type === REPORT_TABS['SCHEDULE'] &&
+                <div>
+                    <Grid item
+                            container
+                            alignItems='center'
+                            direction='row'
+                            justify='flex-start'
+                            >
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={onPreviousClick}
+                                >
+                                Previous
+                            </Button>
+                        </Grid>
+                </div>
                 }
             </div>
         )
     }
 }
+
 export default withStyles(styles)(ReportContent);
--- src/containers/Report.js
+++ src/containers/Report.js
@@ -1,41 +1,117 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {isEqual} from 'lodash';
 import ReportGeneration from '../components/reportGeneration/ReportGeneration';
 import {API_URLS, PROJECT_TABS} from '../constants/Constant';
 import {getApiConfig} from '../services/ApiCofig';
 import {dashboardData} from '../actions/DashboardAction';
 import {projectDetailData} from '../actions/ProjectDataAction';
+import {installationDeviceData} from '../actions/InstallationDeviceData';
+
 
 class Report extends Component {
     state = ({
         tab: 0,
-        project: ''
+        project: '',
+        deviceChecked: [0],
+        page: 0,
+        serviceChecked: [-1]
     });
+
     handleTabChange = (event, value) => {
         this.setState({ tab: value }, function () {
         });
     };
+
     handleProjectSelectionChange = event => {
-        this.setState({project: event.target.value}, function() {
-            this.getLocations();
-        });
+        // Either remove condition and let user select default 'Select Project' Option 
+        // and make an API call with empty value
+        // Or have this condotion added whcih won't user select the default empty option back.
+        if(event.target.value) {
+            this.setState({project: event.target.value}, function() {
+                this.getLocations();
+            });
+        }
     };
+
     getLocations() {
         const endPoint = `${API_URLS['PROJECT_DETAILS']}/${this.state.project}/${PROJECT_TABS['INSTALLATION']}/${PROJECT_TABS['DETAILS']}`,
-        config = getApiConfig(endPoint, 'GET');
-      this.props.onProjectDetailData(config);
+            config = getApiConfig(endPoint, 'GET');
+        this.props.onProjectDetailData(config);
+    };
+
+    handleExpandClick = (index, insid, pid) => {
+        this.setState(state => ({ [index]: !state[index],
+                                    insid : insid }));
+        if(!this.state[insid]) {
+            const endPoint = `${API_URLS['PROJECT_DETAILS']}/${pid}/
+                ${PROJECT_TABS['INSTALLATION']}/${PROJECT_TABS['DEVICES']}/${insid}`,
+                config = getApiConfig(endPoint, 'GET');
+            this.props.onProjectInstallationData(config);
+        }
+    };
+
+    handleDeviceToggle = value => () => {
+        const { deviceChecked } = this.state,
+            newChecked = this.createCheckList(deviceChecked, value);
+        this.setState({
+            deviceChecked: newChecked,
+        });
+    };
+
+    handleServiceToggle = value => () => {
+        const { serviceChecked } = this.state,
+            newChecked = this.createCheckList(serviceChecked, value);
+        this.setState({
+            serviceChecked: newChecked,
+        });
+        
+    };
+
+    createCheckList(checkType, value) {
+        const currentIndex = checkType.indexOf(value),
+        newChecked = [...checkType];
+
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+        return newChecked;
     }
+
+    onNextClick = () => {
+        this.setState({tab: this.state.tab + 1});
+    };
+
+    onPreviousClick = () => {
+        this.setState({tab: this.state.tab - 1});
+    };
+
     componentDidMount(){
         const endPoint = API_URLS['DASHBOARD'],
               config = getApiConfig(endPoint, 'GET');
         this.props.onReport(config);
-      }
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.projectData.InstallationDeviceReducer.data &&
+        !isEqual(this.props.projectData.InstallationDeviceReducer, prevProps.projectData.InstallationDeviceReducer)) {
+            this.setState({[this.state.insid] : this.props.projectData.InstallationDeviceReducer.data[0]});
+        }
+    };
+
     render() {
         return (
-               <ReportGeneration stateData={this.state}
-               handleTabChange={this.handleTabChange}
-               data={this.props.projectData}
-               handleProjectSelectionChange={this.handleProjectSelectionChange}/>
+            <ReportGeneration stateData={this.state}
+            handleTabChange={this.handleTabChange}
+            data={this.props.projectData}
+            handleProjectSelectionChange={this.handleProjectSelectionChange}
+            handleExpandClick={this.handleExpandClick}
+            handleDeviceToggle={this.handleDeviceToggle}
+            onNextClick={this.onNextClick}
+            onPreviousClick={this.onPreviousClick}
+            handleServiceToggle={this.handleServiceToggle}/>
         )
     }
 }
@@ -43,9 +119,9 @@ function mapStateToProps(state) {
     return {
         projectData : state,
     }
-  }
+}
   
-  function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch) {
     return {
         onReport: (config) => {
             //will dispatch the async action
@@ -53,8 +129,11 @@ function mapStateToProps(state) {
         },
         onProjectDetailData: (config) => {
             dispatch(projectDetailData(config))
+        },
+        onProjectInstallationData: (config) => {
+            dispatch(installationDeviceData(config))
         }
     }
-  }
+}
 
 export default (connect)(mapStateToProps, mapDispatchToProps)(Report);
