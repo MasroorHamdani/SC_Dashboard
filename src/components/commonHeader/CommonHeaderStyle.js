3d1c5e0788b2d161941edfe1458bb481079aa528
Masroor Hamdani <masroorhamdani@Masroors-MacBook-Air.local>
2019-04-14 12:06:38 +0800

    bColor change for common header

--- src/components/commonHeader/CommonHeaderStyle.js
+++ src/components/commonHeader/CommonHeaderStyle.js
@@ -6,7 +6,8 @@ const styles = theme => ({
       flexGrow: 1,
     },
     appBar: {
-      background: 'linear-gradient(0.25turn, #fff, #6577de, #3e50b8)',
+      background: 'linear-gradient(0.25turn, #fff, #8db788, #68a554)',
+      // background: 'linear-gradient(0.25turn, #fff, #6577de, #3e50b8)',
     },
     logo: {
       width: '20%'
