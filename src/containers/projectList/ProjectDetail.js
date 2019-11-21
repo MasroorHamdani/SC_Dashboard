8cdd963dd8b1780928d89d7798945190765916d7
Masroor Hamdani <masroorhamdani@yahoo.in>
2019-11-21 18:20:02 +0800

    Fix for existing projects

--- src/containers/projectList/ProjectDetail.js
+++ src/containers/projectList/ProjectDetail.js
@@ -60,13 +60,16 @@ class ProjectDetail extends Component {
                     this.props.projectData.status !== PROJECT_STATUS['REJECT']) {
                     let projectDetail = this.props.projectData;
                     let email= [];
-                    projectDetail.general['ProjectConfig']['HealthUpdates']['Email'].map((row) => {
-                        email.push(Object.values(row)[0])
-                    })
-                    projectDetail.general['Email'] = email.join()
+                    if(projectDetail.general['ProjectConfig'])
+                        projectDetail.general['ProjectConfig']['HealthUpdates']['Email'].map((row) => {
+                            email.push(Object.values(row)[0])
+                        })
+                    projectDetail.general['Email'] = email.join();
                     projectDetail.locations.map((row) => {
-                        row.offtimes[0].Start = formatDateTime(row.offtimes[0].Start, "HHmm", "HH:mm");
-                        row.offtimes[0].End = formatDateTime(row.offtimes[0].End, "HHmm", "HH:mm")
+                        if(!isEmpty(row.offtimes)) {
+                            row.offtimes[0].Start = formatDateTime(row.offtimes[0].Start, "HHmm", "HH:mm");
+                            row.offtimes[0].End = formatDateTime(row.offtimes[0].End, "HHmm", "HH:mm");
+                        }
                     })
                     this.setState({
                         projectDetail: this.props.projectData,
